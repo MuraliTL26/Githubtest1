a746af502e62eb4d287e0633ee1676526f3a96f4
MuraliTL26 <45711126+MuraliTL26@users.noreply.github.com>
2019-03-04 11:23:09 +0530

    Create Sample.java
    commit sample.java file into githubtest1 repo

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample{
+  int num=23;
+  public static void main(String ag[]){
+    System.out.println("Maion method start------------>>>>>>>");
+    System.out.println("Maion method ebd<<<<<<<<------------");
+    }
+  }
