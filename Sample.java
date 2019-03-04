973dc00f2dfd419b0d7839606b14137f16ad1dc0
MuraliTL26 <45711126+MuraliTL26@users.noreply.github.com>
2019-03-04 11:38:31 +0530

    Update Sample.java
    added now in sample class

--- Sample.java
+++ Sample.java
@@ -1,7 +1,8 @@
 public class Sample{
   int num=23;
   public static void main(String ag[]){
-    System.out.println("Maion method start------------>>>>>>>");
+    //added now
+    System.out.println("Maion method start now------------>>>>>>>");
     System.out.println("Maion method ebd<<<<<<<<------------");
     }
   }
