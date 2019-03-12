89322fc9a4292a7fa4330d8ba4c6d05b724c672c
MuraliTL26 <45711126+MuraliTL26@users.noreply.github.com>
2019-03-12 08:20:35 +0530

    Update Sample.java
    simple change

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample{
   int num=23;
   public static void main(String ag[]){
     //added now
-    System.out.println("Maion method start now------------>>>>>>>");
-    System.out.println("Maion method ebd<<<<<<<<------------");
+    System.out.println("Maion method start ------------>>>>>>>");
+    System.out.println("Maion method end<<<<<<<<------------");
     }
   }
