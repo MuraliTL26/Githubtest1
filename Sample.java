cd79f093e3660a3b722e453c3c4ed86c9005d3f2
MuraliTL26 <45711126+MuraliTL26@users.noreply.github.com>
2019-05-05 15:14:25 +0530

    updated sysout
    added print statement

--- Sample.java
+++ Sample.java
@@ -3,6 +3,8 @@ public class Sample{
   public static void main(String ag[]){
     //added now
     System.out.println("Maion method start ------------>>>>>>>");
+    System.out.println("Maion method test purpose ------------>>>>>>>");
+
     System.out.println("Maion method end<<<<<<<<------------");
     }
   }
