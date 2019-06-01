81b896a824ba84470af975a464737ebfa4850327
MuraliTL26 <murali.tl26@gmail.com>
2019-06-01 21:01:02 +0530

    adding sysout statement and pushing into github

--- Sample.java
+++ Sample.java
@@ -2,9 +2,10 @@ public class Sample{
   int num=23;
   public static void main(String ag[]){
     //added now
-    System.out.println("Maion method start ------------>>>>>>>");
+   /* System.out.println("Maion method start ------------>>>>>>>");
     System.out.println("Maion method test purpose ------------>>>>>>>");
     System.out.prinltn("wrong meth");
     System.out.println("Maion method end<<<<<<<<------------");
+   */ System.out.println("pushing code into github");
     }
   }
